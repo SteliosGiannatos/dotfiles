63f70ce6aac4b8b7a96804cf571d0540b32b5903
Stelios Giannatos <github@stelwork.com>
2024-11-12 18:20:33 +0200

    removed omit empty

--- .config/nvim/lua/stelios/lazy/go.lua
+++ .config/nvim/lua/stelios/lazy/go.lua
@@ -1,7 +1,12 @@
 return {
     "crispgm/nvim-go",
     config = function()
+        require('go').setup({
+            tags_name = 'json',
+            tags_options = {}
+        })
         vim.keymap.set('n', '<leader>gtj', ':GoAddTags json<CR>', { noremap = true, silent = true })
         vim.keymap.set('n', '<leader>gty', ':GoAddTags yaml<CR>', { noremap = true, silent = true })
     end
 }
+
