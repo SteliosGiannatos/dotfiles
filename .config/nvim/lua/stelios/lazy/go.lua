740f31be6efc9168e50e9a535de39ac153feee44
Stelios Giannatos <stelios@panadvert.com>
2024-11-14 13:36:06 +0200

    even better mappings for clearing and adding tags, works for both single lines and multiple lines

--- .config/nvim/lua/stelios/lazy/go.lua
+++ .config/nvim/lua/stelios/lazy/go.lua
@@ -5,7 +5,9 @@ return {
             tags_name = 'json',
             tags_options = {}
         })
-        vim.keymap.set('n', '<leader>gtt', ':GoAddTags json,xml,yaml,csv<CR>', { noremap = true, silent = true })
+        vim.keymap.set('n', '<leader>gtt', ":GoAddTags json,xml,yaml,csv<CR>", { noremap = true, silent = true })
+        vim.keymap.set('n', '<leader>gtc', ":GoClearTags<CR>", { noremap = true, silent = true })
+        vim.keymap.set('v', '<leader>gtt', ":'<,'>GoAddTags json,xml,yaml,csv<CR>", { noremap = true, silent = true })
         vim.keymap.set('v', '<leader>gtc', ":'<,'>GoClearTags<CR>", { noremap = true, silent = true })
     end
 }
