f8688bf4da07d8b75e3bea6354a416618687ac29
Stelios Giannatos <github@stelwork.com>
2024-12-11 12:31:36 +0200

    fixed go tags plugin to work with normal and visual mode. telescope works in hidden directories

--- .config/nvim/lua/stelios/lazy/go.lua
+++ .config/nvim/lua/stelios/lazy/go.lua
@@ -6,6 +6,9 @@ return {
             tags_options = {}
         })
         vim.keymap.set('n', '<leader>gtt', ':GoAddTags json,xml,yaml,csv<CR>', { noremap = true, silent = true })
+        vim.keymap.set('v', '<leader>gtt', ':GoAddTags json,xml,yaml,csv<CR>', { noremap = true, silent = true })
+
         vim.keymap.set('v', '<leader>gtc', ":'<,'>GoClearTags<CR>", { noremap = true, silent = true })
+        vim.keymap.set('n', '<leader>gtc', ":'<,'>GoClearTags<CR>", { noremap = true, silent = true })
     end
 }
--- .config/nvim/lua/stelios/lazy/telescope.lua
+++ .config/nvim/lua/stelios/lazy/telescope.lua
@@ -4,10 +4,30 @@ return {
     dependencies = { { 'nvim-lua/plenary.nvim' } },
     config = function()
         local builtin = require('telescope.builtin')
-        vim.keymap.set('n', '<leader>pf', builtin.git_files, {})
-        vim.keymap.set('n', '<c-p>', builtin.find_files, {})
+        vim.keymap.set('n', '<leader>pf', builtin.find_files, {})
+        vim.keymap.set('n', '<c-p>', builtin.git_files, {})
         vim.keymap.set('n', '<leader>ps', function()
             builtin.grep_string({ search = vim.fn.input("Grep > ") });
         end)
+        vim.keymap.set('n', '<leader>vh', builtin.help_tags, {})
+        local telescope = require("telescope")
+        local telescopeConfig = require("telescope.config")
+
+
+        local vimgrepArguments = { unpack(telescopeConfig.values.vimgrep_arguments) }
+        table.insert(vimgrepArguments, "--hidden")
+        table.insert(vimgrepArguments, "--glob")
+        table.insert(vimgrepArguments, "!**/.git/*")
+
+        telescope.setup({
+            defaults = {
+                vimgrep_arguments = vimgrepArguments,
+            },
+            pickers = {
+                find_files = {
+                    find_command = { "rg", "--files", "--hidden", "--glob", "!**/.git/*" },
+                },
+            },
+        })
     end
 }
