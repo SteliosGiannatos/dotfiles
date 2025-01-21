55c536dd4c1ce627a62c093903493c375ac43995
Stelios Giannatos <github@stelwork.com>
2025-01-21 21:34:03 +0200

    transparent vim

--- .config/nvim/lua/stelios/lazy/colors.lua
+++ .config/nvim/lua/stelios/lazy/colors.lua
@@ -1,7 +1,10 @@
 function ColorMyPencils(color)
     color = color or "catppuccin-mocha"
     vim.cmd.colorscheme(color)
+    vim.cmd("highlight Normal guibg=NONE ctermbg=NONE")
+    vim.cmd("highlight NonText guibg=NONE ctermbg=NONE")
 end
+
 return {
     "catppuccin/nvim",
     as = "catppuccin",
