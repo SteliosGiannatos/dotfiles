f97bd005f6b1d56a8ad52a7a2ff99bebb46f65d5
stelios <github@stelwork.com>
2026-04-01 02:02:16 +0300

    removed media files. Uses latest version

--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -1,7 +1,11 @@
 return {
     'nvim-telescope/telescope.nvim',
-    version = '0.1.6',
-    dependencies = { { 'nvim-lua/plenary.nvim' } },
+    version = '*',
+    dependencies = {
+        'nvim-lua/plenary.nvim',
+        -- optional but recommended
+        { 'nvim-telescope/telescope-fzf-native.nvim', build = 'make' },
+    },
     config = function()
         local builtin = require('telescope.builtin')
         vim.keymap.set('n', '<leader>pf', builtin.find_files, {})
