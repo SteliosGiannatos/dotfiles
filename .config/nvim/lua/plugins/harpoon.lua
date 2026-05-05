83b4ab7f2cc8864b12ba73da5e781c2b12a794ac
stelios <github@stelwork.com>
2026-05-06 00:36:58 +0300

    latest harpoon version. Harpoon uses telescope UI

--- .config/nvim/lua/plugins/harpoon.lua
+++ .config/nvim/lua/plugins/harpoon.lua
@@ -1,28 +1,50 @@
 return {
-    'ThePrimeagen/harpoon',
+    "ThePrimeagen/harpoon",
+    branch = "harpoon2",
+    dependencies = {
+        "nvim-lua/plenary.nvim",
+        "nvim-telescope/telescope.nvim",
+    },
     config = function()
-        local mark = require("harpoon.mark")
-        local ui = require("harpoon.ui")
+        local harpoon = require("harpoon")
+        harpoon:setup()
 
+        -- fix: telescope picker for harpoon list
+        local function toggleTelescopeHarpoon(harpoonFiles)
+            local conf = require("telescope.config").values
+            local filePaths = {}
+            for _, item in ipairs(harpoonFiles.items) do
+                table.insert(filePaths, item.value)
+            end
 
-        vim.keymap.set("n", "<leader>a", mark.add_file)
-        vim.keymap.set("n", "<leader>e", ui.toggle_quick_menu)
+            require("telescope.pickers").new({}, {
+                prompt_title = "Harpoon",
+                finder = require("telescope.finders").new_table({
+                    results = filePaths,
+                }),
+                previewer = conf.file_previewer({}),
+                sorter = conf.generic_sorter({}),
+            }):find()
+        end
 
+        vim.keymap.set("n", "<leader>a", function() harpoon:list():add() end)
+        -- fix: open telescope picker instead of default quick menu
+        vim.keymap.set("n", "<leader>e", function() toggleTelescopeHarpoon(harpoon:list()) end)
 
         vim.keymap.set("n", "<C-h>", function()
-            ui.nav_file(1)
+            harpoon:list():select(1)
             vim.cmd("normal! zz")
         end)
         vim.keymap.set("n", "<C-j>", function()
-            ui.nav_file(2)
+            harpoon:list():select(2)
             vim.cmd("normal! zz")
         end)
         vim.keymap.set("n", "<C-k>", function()
-            ui.nav_file(3)
+            harpoon:list():select(3)
             vim.cmd("normal! zz")
         end)
         vim.keymap.set("n", "<C-l>", function()
-            ui.nav_file(4)
+            harpoon:list():select(4)
             vim.cmd("normal! zz")
         end)
     end,
